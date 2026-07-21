2afc1af07bb7cf4e429faa713291ee8e13c8d291
harshitg123 <harshit.28.sps@gmail.com>
2026-07-21 23:06:26 +0530

    more detaling added

--- js/app.js
+++ js/app.js
@@ -900,6 +900,13 @@ finalBtn.addEventListener("click", () => {
 closeLetter.addEventListener("click", () => {
   endingCard.classList.remove("show");
 });
+
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX / window.innerWidth - 0.5;
+  const y = e.clientY / window.innerHeight - 0.5;
+
+  moon.style.transform = `translate(${x * 10}px, ${y * 10}px)`;
+});
 /* ==========================================================
    START APPLICATION
 ========================================================== */
